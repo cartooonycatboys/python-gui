697dfbb386720a1e78756d52b7b666a031813c81
cartooonycatboys <cimsitruzgar@gmail.com>
2026-05-08 19:56:12 +0300

    Add files via upload

---
+++ Lesson 3/frame.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter.ttk import *
+
+root=Tk()
+root.geometry("300x150")
+w=Label(root,text="chocos and icecreames",font="50")
+w.pack()
+frame=Frame(root)
+frame.pack()
+root.mainloop()
